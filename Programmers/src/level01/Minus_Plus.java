4299daa4d3704e9eda381750fb27c63a38f189a6
start-27 <minw7092@naver.com>
2022-02-02 23:52:36 +0900

    음양 더하기

---
+++ Programmers/src/level01/Minus_Plus.java
@@ -0,0 +1,21 @@
+package level01;
+
+public class Minus_Plus {
+    public static void main(String[] args) {
+        int[] absolutes = {4, 7, 12};
+        boolean[] signs = {true, false, true};
+        System.out.println(Solution(absolutes, signs));
+    }
+    private static int Solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer += (-1)*absolutes[i];
+            }
+        }
+        
+        return answer;
+    }
+}
