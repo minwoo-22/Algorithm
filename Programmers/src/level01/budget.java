fc516f0d82aedf13f4ce37be5aca122180d1075a
start-27 <minw7092@naver.com>
2022-02-04 16:11:34 +0900

    예산

---
+++ Programmers/src/level01/budget.java
@@ -0,0 +1,24 @@
+package level01;
+
+import java.util.Arrays;
+
+public class budget {
+    public static void main(String[] args) {
+        int[] d = {2,2,3,3};
+        int budget = 10;
+        int answer = solution(d, budget);
+        System.out.println(answer);
+    }
+    
+    private static int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+            if (budget < 0) break;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
