4e065a3cd6df0d287dfd363db9f2f527ccd46047
start-27 <minw7092@naver.com>
2022-02-06 00:09:20 +0900

    모의고사

---
+++ Programmers/src/level01/SuPoja2.java
@@ -0,0 +1,46 @@
+package level01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SuPoja2 {
+    public static void main(String[] args) {
+        int[] answers = {4,1,3,1,5};
+        int[] answer = solution(answers);
+        System.out.println(Arrays.toString(answer));
+    }
+    
+    public static int[] solution(int[] answers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] scores = {0,0,0};
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (first[i % 5] == answers[i]) scores[0]++;
+            if (second[i % 8] == answers[i]) scores[1]++;
+            if (third[i % 10] == answers[i]) scores[2]++;
+        }
+        
+        int[] arr = new int[scores.length];
+        for (int i = 0; i < scores.length; i++) {
+            arr[i] = scores[i];
+        }
+        
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (scores[i] == arr[2]) list.add(i+1);
+        }
+        
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
