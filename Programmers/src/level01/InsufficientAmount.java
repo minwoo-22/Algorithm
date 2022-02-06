2d39ac6799e6f6577a986683e835baa9737f415b
start-27 <minw7092@naver.com>
2022-02-07 02:35:41 +0900

    부족한 금액 계산

---
+++ Programmers/src/level01/InsufficientAmount.java
@@ -0,0 +1,24 @@
+package level01;
+
+public class InsufficientAmount {
+
+    public static void main(String[] args) {
+        int price = 3;
+        int money = 20;
+        int count = 4;
+        long result = solution(price, money, count);
+        System.out.println(result);
+    }
+    
+    private static long solution(int price, int money, int count) {
+        long answer = (long)money;
+        for (int i = 0; i < count; i++) {
+            answer -= (long)price * (i+1);
+        }
+        if (answer >= 0) {
+            answer = 0;
+        }
+        return Math.abs(answer);
+    }
+
+}
