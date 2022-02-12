0ec819b17e7a43b46df04dc33917b651cfc46299
start-27 <minw7092@naver.com>
2022-02-12 20:58:19 +0900

    같은 숫자는 싫어

---
+++ Programmers/src/level01/HateSameNumber.java
@@ -0,0 +1,32 @@
+package level01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HateSameNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        int[] answer = solution(arr);
+        System.out.println(Arrays.toString(answer));
+    }
+    
+    private static int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+                
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] != arr[i]) list.add(arr[i]);
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+
+}
