11982b1dac7f4afcb4f85c0b9e00ed67fbb30f15
start-27 <minw7092@naver.com>
2022-02-13 23:33:05 +0900

    수박수박수박수?

---
+++ Programmers/src/level01/WaterMelon.java
@@ -0,0 +1,24 @@
+package level01;
+
+public class WaterMelon {
+
+    public static void main(String[] args) {
+        int n = 3;
+        String answer = solution(n);
+        System.out.println(answer);
+    }
+    
+    private static String solution(int n) {
+        String answer = "";
+        String s = "수박";
+        int index = 0;
+        
+        for (int i = 0; i < n; i++) {
+            index = i % 2;
+            answer += s.charAt(index);
+        }
+        
+        return answer;
+    }
+
+}
