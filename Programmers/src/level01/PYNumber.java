cc3ec8584f73023c4d7724b38e3b5b6f3ce363e8
start-27 <minw7092@naver.com>
2022-02-13 21:47:02 +0900

    문자열 내 p와 y의 개수

---
+++ Programmers/src/level01/PYNumber.java
@@ -0,0 +1,29 @@
+package level01;
+
+public class PYNumber {
+
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+        boolean answer = solution(s);
+        System.out.println(answer);
+    }
+    
+    private static boolean solution(String s) {
+        boolean answer = true;
+        int p = 0;
+        int y = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                p += 1;
+            }
+            if (s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                y += 1;
+            }
+        }
+        
+        if (p != y) answer = false;
+        return answer;
+    }
+
+}
