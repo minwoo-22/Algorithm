8e0daedc762626b4d49db934c6863c8a2cf30b47
start-27 <minw7092@naver.com>
2022-02-13 22:03:41 +0900

    문자열 기본

---
+++ Programmers/src/level01/StringBasic.java
@@ -0,0 +1,23 @@
+package level01;
+
+public class StringBasic {
+
+    public static void main(String[] args) {
+        String s = "a123";
+        boolean answer = solution(s);
+        System.out.println(answer);
+    }
+    
+    private static boolean solution(String s) {
+        boolean answer = true;
+        
+        if (s.length() != 4 && s.length() != 6) return false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) > '9') return false;
+        }
+        
+        return answer;
+    }
+
+}
