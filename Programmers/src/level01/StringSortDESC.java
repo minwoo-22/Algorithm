93b7774c0f929735ec59ddd236c24052dfd49147
start-27 <minw7092@naver.com>
2022-02-13 21:55:56 +0900

    문자열 내림차순으로 배치하기

---
+++ Programmers/src/level01/StringSortDESC.java
@@ -0,0 +1,29 @@
+package level01;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class StringSortDESC {
+
+    public static void main(String[] args) {
+        String s = "Zbcdefg";
+        String answer = solution(s);
+        System.out.println(answer);
+    }
+    
+    private static String solution(String s) {
+        String answer = "";
+        Character[] str = new Character[s.length()];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = s.charAt(i);
+        }
+        Arrays.sort(str, Collections.reverseOrder());
+        
+        for (int i = 0; i < str.length; i++) {
+            answer += str[i];
+        }
+        
+        return answer;
+    }
+
+}
