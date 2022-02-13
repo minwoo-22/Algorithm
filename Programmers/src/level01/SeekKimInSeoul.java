9248f89f7d98443d231ef992f3694324e7a5ab9d
start-27 <minw7092@naver.com>
2022-02-13 22:09:24 +0900

    서울에서 김서방 찾기

---
+++ Programmers/src/level01/SeekKimInSeoul.java
@@ -0,0 +1,26 @@
+package level01;
+
+public class SeekKimInSeoul {
+
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        String answer = solution(seoul);
+        System.out.println(answer);
+    }
+    
+    private static String solution(String[] seoul) {
+        String answer = "";
+        int index = 0;
+        
+        for (int i = 0; i < seoul.length; i++) {
+            if ("Kim".equals(seoul[i])) {
+                index = i;
+            }
+        }
+        
+        answer = "김서방은 "+index+"에 있다";
+        
+        return answer;
+    }
+
+}
