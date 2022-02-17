da3c217642d18b44b07329e0d06ab5306f68a17a
start-27 <minw7092@naver.com>
2022-02-17 22:06:11 +0900

    제일 작은 수 제거하기

---
+++ Programmers/src/level01/RemoveSmallestNumber.java
@@ -0,0 +1,34 @@
+package level01;
+
+import java.util.Arrays;
+
+public class RemoveSmallestNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {10};
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+    
+    private static int[] solution(int[] arr) {
+        int[] answer = new int[arr.length-1];
+        
+        if (arr.length == 1) {
+            return new int[] {-1};
+        }
+        
+        int smallest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == smallest) continue;
+            answer[j] = arr[i];
+            j++;
+        }
+        return answer;
+    }
+
+}
