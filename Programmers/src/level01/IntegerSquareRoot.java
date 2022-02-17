99ca48632e314447a4474ad127697f17f1c76425
start-27 <minw7092@naver.com>
2022-02-17 21:43:25 +0900

    정수 제곱근 판별

---
+++ Programmers/src/level01/IntegerSquareRoot.java
@@ -0,0 +1,24 @@
+package level01;
+
+public class IntegerSquareRoot {
+
+    public static void main(String[] args) {
+        long n = 121;
+        long answer = solution(n);
+        System.out.println(answer);
+    }
+    
+    private static long solution(long n) {
+        long answer = 0;
+        double result = Math.sqrt(n);
+        if ((result*10.0)%10.0 == 0.0) {
+            answer = (long)Math.pow(result+1, 2.0);
+        } else {
+            answer = -1;
+        }
+        
+        return answer;
+    }
+    
+
+}
