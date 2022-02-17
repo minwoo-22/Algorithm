d93f2f70a96426c804609fa65bf12a614039515e
start-27 <minw7092@naver.com>
2022-02-17 21:10:29 +0900

    정수 내림차순으로 배치하기

---
+++ Programmers/src/level01/IntegerDescendingOrder.java
@@ -0,0 +1,31 @@
+package level01;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class IntegerDescendingOrder {
+
+    public static void main(String[] args) {
+        long n = 118372;
+        long answer = solution(n);
+        System.out.println(answer);
+    }
+    
+    private static long solution(long n) {
+        long answer = 0;
+        String number = String.valueOf(n);
+        Long[] arr = new Long[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            arr[i] = Long.parseLong(number.charAt(i)+"");
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        
+        String sortNumber = "";
+        for (int i = 0; i < arr.length; i++) {
+            sortNumber += arr[i];
+        }
+        
+        answer = Long.parseLong(sortNumber);
+        return answer;
+    }
+}
