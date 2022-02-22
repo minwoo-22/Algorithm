e9d3c77df33225b66995474861c0ef8c28595779
start-27 <minw7092@naver.com>
2022-02-22 22:22:33 +0900

    콜라츠 추측

---
+++ Programmers/src/level01/Collatz.java
@@ -0,0 +1,34 @@
+package level01;
+
+public class Collatz {
+    public static void main(String[] args) {
+        long n = 626331;
+        long answer = solution(n);
+        System.out.println(answer);
+    }
+    
+    private static long solution(long n) {
+        long answer = 0;
+        
+        int cnt = 0;
+        while(cnt < 500) {
+            if (n%2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            cnt++;
+            if (n == 1) break;
+        }
+        if (cnt == 500) {
+            answer = -1;
+        } else {
+            answer = cnt;
+        }
+        System.out.println(n);
+        System.out.println(cnt);
+        return answer;
+    }
+    
+    
+}
