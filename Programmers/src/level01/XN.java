be2ae7bc7d031ea440aae94e1dcdfa47bbe96562
start-27 <minw7092@naver.com>
2022-02-24 01:14:56 +0900

    X만큼 간격이 있는 n개의 숫자

---
+++ Programmers/src/level01/XN.java
@@ -0,0 +1,27 @@
+package level01;
+
+import java.util.Arrays;
+
+public class XN {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        long[] answer = solution(x, n);
+        System.out.println(Arrays.toString(answer));
+    }
+    
+    private static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        
+        int j = 0;
+        long result = 0;
+        for (int i = 1; i <= n; i++) {
+            answer[j] = result+x;
+            result = answer[j];
+            j++;
+        }
+        
+        return answer;
+    }
+}
