68563660c00d0a0b589b5df7e897c8e01f319da5
start-27 <minw7092@naver.com>
2022-02-24 23:10:51 +0900

    [1차] 다트 게임

---
+++ Programmers/src/level01/Dart_Game.java
@@ -0,0 +1,55 @@
+package level01;
+
+import java.util.Arrays;
+
+public class Dart_Game {
+    public static void main(String[] args) {
+        String dartResult = "1D2S#10S";
+        System.out.println(solution(dartResult));
+    }
+    
+    private static int solution(String dartResult) {
+        int answer = 0;
+        
+        int[] numbers = new int[3];
+        int length = dartResult.length();
+        
+        int j = 0;
+        for (int i = 0; i < length; i++) {
+            char c = dartResult.charAt(i);
+            
+            if (c >= '1' && c <= '9') {
+                if (dartResult.charAt(i+1) == '0') {
+                    numbers[j] = 10;
+                    i++;
+                } else {
+                    numbers[j] = c - '0';
+                }
+            } else if (c == 'D') {
+                numbers[j] = (int) Math.pow(numbers[j], 2);
+                j++;
+            } else if (c == 'T') {
+                numbers[j] = (int) Math.pow(numbers[j], 3);
+                j++;
+            } else if (c == 'S') {
+                j++;
+            } else if (c == '*') {
+                if (j-1 >= 0) {
+                    numbers[j-1] *= 2;
+                }
+                if (j-2 >= 0) {
+                    numbers[j-2] *= 2;
+                }
+            } else if (c == '#') {
+                numbers[j-1] *= -1;
+            }
+        }
+        
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+            answer += numbers[i];
+        }
+        
+        return answer;
+    }
+}
