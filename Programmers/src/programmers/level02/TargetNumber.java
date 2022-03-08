8ac1dd722abd4f8efcfda7cd50183ac4d3c0bfbf
start-27 <minw7092@naver.com>
2022-03-08 21:46:44 +0900

    타겟 넘버

---
+++ Programmers/src/programmers/level02/TargetNumber.java
@@ -0,0 +1,31 @@
+package programmers.level02;
+
+public class TargetNumber {
+    static int result = 0;
+    
+    public static void main(String[] args) {
+        int[] numbers = {4,1,2,1};
+        int target = 4;
+        int answer = solution(numbers, target);
+        System.out.println(answer);
+    }
+    
+    private static int solution(int[] numbers, int target) {
+        int answer = 0;
+        method(numbers, 1, numbers[0], target);
+        method(numbers, 1, -numbers[0], target);
+        
+        System.out.println("result : " + result);
+        answer = result;
+        return answer;
+    }
+    
+    private static void method(int[] numbers, int n, int answer, int target) {
+        if (n == numbers.length) {
+            if (answer == target) result++;
+        } else {
+            method(numbers, n+1, answer+numbers[n], target);
+            method(numbers, n+1, answer-numbers[n], target);
+        }
+    }
+}
