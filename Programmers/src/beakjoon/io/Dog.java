d32161ad43b44b05ac9aa0c0c2780bab43f6fd11
start-27 <minw7092@naver.com>
2022-07-25 23:24:21 +0900

    입출력과 사칙연산

---
+++ Programmers/src/beakjoon/io/Dog.java
@@ -0,0 +1,17 @@
+package beakjoon.io;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        String[] strs = {"|\\_/|"
+                , "|q p|   /}"
+                , "( 0 )\"\"\"\\"
+                , "|\"^\"`    |"
+                , "||_/=\\\\__|"};
+        
+        for (int i = 0; i < strs.length; i++) {
+            System.out.println(strs[i]);
+        }
+    }
+
+}
