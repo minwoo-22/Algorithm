2a32624a91d31cfbbf261bcf7ce3e65a1c922fbc
start-27 <minw7092@naver.com>
2022-08-06 16:59:26 +0900

    입출력과 사칙연산(3)

---
+++ Programmers/src/beakjoon/io/Multiplication.java
@@ -0,0 +1,28 @@
+package beakjoon.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Multiplication {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int firstNum = Integer.parseInt(br.readLine());
+        int secondNum = Integer.parseInt(br.readLine());
+        
+        // 두 번째 숫자의 백의 자리, 십의 자리, 일의 자리를 각각 구한다.
+        // 일의 자리
+        int A = secondNum % 10;
+        // 십의 자리
+        int B = secondNum % 100 / 10;
+        // 백의 자리
+        int C = secondNum % 1000 / 100;
+        
+        System.out.println(firstNum * A);
+        System.out.println(firstNum * B);
+        System.out.println(firstNum * C);
+        System.out.println(firstNum * secondNum);
+    }
+
+}
---
+++ Programmers/src/beakjoon/io/Sprout.java
@@ -0,0 +1,14 @@
+package beakjoon.io;
+
+public class Sprout {
+    public static void main(String[] args) {
+        String[] strs = {
+          "         ,r'\"7", "r`-_   ,'  ,/", " \\. \". L_r'"
+          , "   `~\\/", "      |", "      |"
+        };
+        
+        for (int i = 0; i < strs.length; i++) {
+            System.out.println(strs[i]);
+        }
+    }
+}
