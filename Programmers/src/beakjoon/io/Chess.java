78c39699778caec8bc23767c41e1107f7760aba5
start-27 <minw7092@naver.com>
2022-08-07 18:59:06 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ Programmers/src/beakjoon/io/Chess.java
@@ -0,0 +1,28 @@
+package beakjoon.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Chess {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] chessNum = {1, 1, 2, 2, 2, 8};
+        int[] answer = new int[chessNum.length];
+        
+        for (int i = 0; i < chessNum.length; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            
+            answer[i] = chessNum[i] - num;
+        }
+        
+        for (int num : answer) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
