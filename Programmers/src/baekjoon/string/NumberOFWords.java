b61b27b9fafd578a8a9d0bc9488732dba0c6b1f1
start-27 <minw7092@naver.com>
2022-08-13 13:01:33 +0900

    문자열(3)

--- Programmers/src/baekjoon/string/NumberOFWords.java
+++ Programmers/src/baekjoon/string/NumberOFWords.java
@@ -15,10 +15,6 @@ public class NumberOFWords {
         str = str.trim();
         String[] words = str.split(" ");
         
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
-        }
-        
         if (str.isEmpty()) {
             System.out.println(0);
         } else {
