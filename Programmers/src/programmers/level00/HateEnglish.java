d2d35ff81896be792566465c706a7937f6e42066
start-27 <minw7092@naver.com>
2023-03-05 18:42:38 +0900

    나는 영어가 싫어요

---
+++ Programmers/src/programmers/level00/HateEnglish.java
@@ -0,0 +1,14 @@
+package programmers.level00;
+
+public class HateEnglish {
+    public static void main(String[] args) {
+        String numbers = "onetwothreefourfivesixseveneightnine";
+        numbers = numbers.replaceAll("one", "1").replaceAll("two", "2")
+                .replaceAll("three", "3").replaceAll("four", "4")
+                .replaceAll("five", "5").replaceAll("six", "6")
+                .replaceAll("seven", "7").replaceAll("eight", "8")
+                .replaceAll("nine", "9").replaceAll("zero", "0");
+
+        System.out.println(Long.valueOf(numbers));
+    }
+}
